de6b7ae66126e13753b9904602e3188dc1e67354
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-29 20:00:06 +0300

    Create variables.tf

---
+++ terr-setup/variables.tf
@@ -0,0 +1,20 @@
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "sa_name" {
+  description = "Service account name"
+  type        = string
+  default     = "terr-sa"
+}
