b1eaafd89b491429ce38cfe768d05b3a899b629f
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-30 22:37:59 +0300

    Update backend.tf

--- terr-infra/backend.tf
+++ terr-infra/backend.tf
@@ -1,13 +1,3 @@
-# terr-setup/backend.tf
-terraform {
-  backend "remote" {
-    organization = "JulieJool"
-    workspaces {
-      name = "terr-setup"
-    }
-  }
-}
-
 # terr-infra/backend.tf
 terraform {
   backend "remote" {
