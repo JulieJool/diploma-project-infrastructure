0ef53aeea8ad8e608e8d59b8944f102da4163cdc
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-30 15:40:27 +0300

    Update bucket.tf

--- terr-setup/bucket.tf
+++ terr-setup/bucket.tf
@@ -7,12 +7,3 @@ resource "yandex_storage_bucket" "terraform-state-bucket" {
   force_destroy = true
   depends_on = [yandex_resourcemanager_folder_iam_member.editor-sa]
 }
-
-resource "local_file" "credfile" {
-  filename = "../terr-infra/creddata.key"
-  content  = <<-EOT
-    [default]
-    aws_access_key_id = ${yandex_iam_service_account_static_access_key.terr-sa-static-key.access_key}
-    aws_secret_access_key = ${yandex_iam_service_account_static_access_key.terr-sa-static-key.secret_key}
-  EOT
-}
