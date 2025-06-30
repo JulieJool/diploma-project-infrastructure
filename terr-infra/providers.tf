75d479d4af41a2c3f9ee8f57daff88c3b3ffd648
JulieJool <150677323+JulieJool@users.noreply.github.com>
2025-06-30 21:33:41 +0300

    Update providers.tf

--- terr-infra/providers.tf
+++ terr-infra/providers.tf
@@ -26,5 +26,5 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.default_zone
-  service_account_key_file = var.service_account_key_json
+  service_account_key_file = base64decode(var.sa_key_b64)
 }
